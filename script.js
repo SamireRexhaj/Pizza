3fa292a84d23becae69b09b190f0d279b7145881
SamireRexhaj <samire.rexhaj@universitetiaab.com>
2024-01-15 15:09:48 +0100

    Added Files

---
+++ script.js
@@ -0,0 +1,54 @@
+let cart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart').onclick = () =>{
+  cart.classList.toggle('active');
+  login.classList.remove('active');
+}
+
+let login = document.querySelector('.login-form');
+
+document.querySelector('#login').onclick = () =>{
+  login.classList.toggle('active');
+  cart.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu').onclick = () =>{
+  navbar.classList.toggle('active');
+  cart.classList.remove('active');
+  login.classList.remove('active');
+}
+
+window.onscroll = () =>{
+  cart.classList.remove('active');
+  login.classList.remove('active');
+  navbar.classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+  autoplay:{
+    delay:7500,
+    disableOnInteraction:false,
+  },
+  grabCursor:true,
+  loop: true,
+  centeredSlides:true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+var swiper = new Swiper(".menu-slider", {
+  grabCursor:true,
+  loop: true,
+  autoHeight:true,
+  centeredSlides:true,
+  spaceBetwwen:20,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+  },
+});
